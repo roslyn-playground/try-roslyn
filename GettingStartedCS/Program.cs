dabf7a0c45e477044dae30555a6e8c5a3dab0649
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-08-25 01:41:48 +0700

    Traversing trees

---
+++ GettingStartedCS/Program.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GettingStartedCS 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = CSharpSyntaxTree.ParseText(
+@" using System;
+using System.Collections;
+using Systme.Linq;
+using System.Text;
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(String[] args) 
+        { 
+            Console.WriteLine(""Hello, world!"");
+        }
+    }
+}
+");
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var firstMember = root.Members[0];
+            var helloWorldDeclaration = (NamespaceDeclarationSyntax)firstMember;
+            var programDeclaration = (ClassDeclarationSyntax)helloWorldDeclaration.Members[0];
+            var mainDeclaration = (MethodDeclarationSyntax)programDeclaration.Members[0];
+            var argsParameter = mainDeclaration.ParameterList.Parameters[0];
+
+            var firstParameters = from methodDeclaration in root.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                                  where methodDeclaration.Identifier.ValueText == "Main"
+                                  select methodDeclaration.ParameterList.Parameters.First();
+
+            var argsParameter2 = firstParameters.Single(); 
+
+        }
+    }
+}
